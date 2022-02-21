11742ae0ea790b092dbb681751b878027a55fd80
ZE4N <99141753+ze4n-github@users.noreply.github.com>
2022-02-20 20:03:58 -0600

    Add files via upload

---
+++ IPTRACK.py
@@ -0,0 +1,58 @@
+import requests
+import json
+import os
+import time
+BLACK = '\033[30m'
+RED = '\033[31m'
+GREEN = '\033[32m'
+YELLOW = '\033[33m'
+BLUE = '\033[34m'
+MAGENTA = '\033[35m'
+CYAN = '\033[36m'
+WHITE = '\033[37m'
+RESET = '\033[39m'
+BLACK = '\033[30m'
+os.system("clear")
+print(RED+"██╗██████╗░  ████████╗██████╗░░█████╗░░█████╗░██╗░░██╗")
+print("██║██╔══██╗  ╚══██╔══╝██╔══██╗██╔══██╗██╔══██╗██║░██╔╝")
+print("██║██████╔╝  ░░░██║░░░██████╔╝███████║██║░░╚═╝█████═╝░")
+print("██║██╔═══╝░  ░░░██║░░░██╔══██╗██╔══██║██║░░██╗██╔═██╗░")
+print("██║██║░░░░░  ░░░██║░░░██║░░██║██║░░██║╚█████╔╝██║░╚██╗")
+print("╚═╝╚═╝░░░░░  ░░░╚═╝░░░╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝\n \n")
+time.sleep(1)
+print(YELLOW+"--------------------------------------")
+time.sleep(1)
+print("Autor    : ZE4N")
+time.sleep(1)
+print("--------------------------------------")
+time.sleep(1)
+print("YouTube  : ZE4NCITO ")
+time.sleep(1)
+print("--------------------------------------")
+time.sleep(1)
+print("INSTAGRAM: ze4n.py")
+time.sleep(1)
+print("--------------------------------------")
+time.sleep(1)
+print("TIK TOK  : zean.py")
+time.sleep(1)
+print("-------------------------------------- \n")
+
+api_url = "http://ip-api.com/json/"
+parametros = 'status,country,countryCode,region,regionName,city,zip,lat,lon,timezone,isp,org,as,query'
+data = {"fields":parametros}
+
+def ip_scraping(ip=""):
+ res = requests.get(api_url+ip, data=data)
+ api_json_res = json.loads(res.content)
+ return api_json_res
+
+if __name__ == '__main__':
+ ip = input(CYAN+"Ingrese la dirección IP: "+GREEN)
+
+
+par = parametros.split(",")
+for x in par:
+ print(x.upper(), ":")
+ print( ip_scraping(ip)[x])
+ print("===================================")
